6789573493bf724f9fcd04200c793202c38682fd
SabrinaAina <sabrinainadzulkifli@gmail.com>
2023-05-08 16:54:51 +0800

    Revert "Lab 4 Commit"
    This reverts commit 68f56ab074a79b9937f5bca6e8568789e475d24c.

--- restorderapp/src/main/java/my/edu/utem/ftmk/dad/restorderapp/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package my.edu.utem.ftmk.dad.restorderapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import my.edu.utem.ftmk.dad.restorderapp.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
